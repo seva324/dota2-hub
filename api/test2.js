52fac3d5004da053b8f86d8c689b7a4d380c7836
seva324 <seva324@github.com>
2026-02-24 08:51:02 +0800

    fix test2 signature

--- api/test2.js
+++ api/test2.js
@@ -1,8 +1,8 @@
 /**
  * Test API
  */
-export default async function handler(req, res) {
-  return res.status(200).json({ 
+export default async function handler(response) {
+  return response.status(200).json({ 
     ok: true,
     redis: process.env.REDIS_URL ? 'SET' : 'NOT_SET'
   });
